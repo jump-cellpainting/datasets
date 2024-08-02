4ceadcfac8e6ca5663bc43e46292170beffa581d
Alan Munoz <afer.mg@gmail.com>
2024-08-02 13:56:05 -0400

    style: fix indentation and comment

--- src/update_etags.sh
+++ src/update_etags.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Returns the updated ETag for elements in the second column of $1 alongside the first two columns.
 cat $1 |
-    tail -n +2 | # Remove headers
+ tail -n +2 | # Remove headers
  cut -f2 -d',' | # Select url column
  xargs -I {} -- curl -I --silent "{}" | # Fetch remote metadata 
- grep "ETag" |  # Select and clean etags
+ grep "ETag" |  # Select etag field from resulting html
  awk '{print $2}' | # Remove prefix
  sed 's/\r$//' | # Remove carriage
  sed 1i'"etag"' | # add header
