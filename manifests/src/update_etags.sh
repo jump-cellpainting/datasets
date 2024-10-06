4b646d246418dedfab4661aaf9609485c2dadacc
Shantanu Singh <shsingh@broadinstitute.org>
2024-10-06 13:24:23 -0400

    cleanup

--- manifests/src/update_etags.sh
+++ manifests/src/update_etags.sh
@@ -12,7 +12,7 @@ fi
 url_column=$(head -n1 "${input_file}" | tr ',' '\n' | grep -n "url" | cut -d':' -f1)
 urls=$(awk -F',' -v col="${url_column}" 'NR>1 {gsub(/^"|"$/, "", $col); print $col}' "${input_file}")
 
-# Fetch ETags for each URL in a loop to avoid xargs command length issues
+# Fetch ETags for each URL in a loop 
 etag_values="etag"
 while IFS= read -r url; do
   etag=$(curl -I --silent "${url}" | awk '/[eE][tT]ag:/ {print $2}' | tr -d '\r')
@@ -22,7 +22,6 @@ done <<< "$urls"
 # Remove existing ETag column if present
 etag_column=$(head -n1 "${input_file}" | tr ',' '\n' | grep -n "etag" | cut -d':' -f1)
 
-# Remove existing ETag column if present using cut
 if [[ -n "$etag_column" ]]; then
     stripped_data=$(cut -d',' -f"1-$((etag_column-1)),$((etag_column+1))-" "${input_file}")
 else
