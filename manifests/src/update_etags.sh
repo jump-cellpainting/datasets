eaa24f1db00be3c84177b0bd11860f07f717812b
Alán F. Muñoz <afer.mg@gmail.com>
2024-10-07 20:10:56 -0400

    format: apply shfmt to scripts

--- manifests/src/update_etags.sh
+++ manifests/src/update_etags.sh
@@ -5,8 +5,8 @@ input_file="$1"
 
 # Check if input file is provided
 if [ -z "${input_file}" ]; then
-  echo "Usage: $0 <input_file>"
-  exit 1
+	echo "Usage: $0 <input_file>"
+	exit 1
 fi
 
 url_column=$(head -n1 "${input_file}" | tr ',' '\n' | grep -n "url" | cut -d':' -f1)
@@ -15,17 +15,17 @@ urls=$(awk -F',' -v col="${url_column}" 'NR>1 {gsub(/^"|"$/, "", $col); print $c
 # Fetch ETags for each URL in a loop
 etag_values="etag"
 while IFS= read -r url; do
-  etag=$(curl -I --silent "${url}" | awk '/ETag:/ {print $2}' | tr -d '\r')
-  etag_values+="\n${etag}"
-done <<< "$urls"
+	etag=$(curl -I --silent "${url}" | awk '/ETag:/ {print $2}' | tr -d '\r')
+	etag_values+="\n${etag}"
+done <<<"$urls"
 
 # Remove existing ETag column if present
 etag_column=$(head -n1 "${input_file}" | tr ',' '\n' | grep -n "etag" | cut -d':' -f1)
 
 if [[ -n "$etag_column" ]]; then
-    stripped_data=$(cut -d',' -f"1-$((etag_column-1)),$((etag_column+1))-" "${input_file}")
+	stripped_data=$(cut -d',' -f"1-$((etag_column - 1)),$((etag_column + 1))-" "${input_file}")
 else
-    stripped_data=$(cat "${input_file}")
+	stripped_data=$(cat "${input_file}")
 fi
 
 # Combine original data (without ETag) with new ETag values
--- manifests/src/upload_index.sh
+++ manifests/src/upload_index.sh
@@ -31,9 +31,9 @@ local_etags=$(awk -F',' -v col="${etag_column}" 'NR>1 {gsub(/^"|"$/, "", $col); 
 
 s3_etags=""
 while IFS= read -r url; do
-  etag=$(curl -I --silent "$url" | awk '/[eE][tT]ag:/ {print $2}' | tr -d '\r"')
-  s3_etags+="${etag}\n"
-done <<< "${urls}"
+	etag=$(curl -I --silent "$url" | awk '/[eE][tT]ag:/ {print $2}' | tr -d '\r"')
+	s3_etags+="${etag}\n"
+done <<<"${urls}"
 
 # Remove the trailing newline from s3_etags
 s3_etags=$(echo -e "${s3_etags}" | sed '/^$/d')
@@ -44,45 +44,44 @@ local_etags_hash=$(echo "${local_etags}" | md5sum | cut -f1 -d" ")
 
 echo "Remote ${s3_etags_hash} vs Local ${local_etags_hash} values"
 if [ "${s3_etags_hash}" != "${local_etags_hash}" ]; then
-    echo "At least one ETag does not match their url."
-    exit 1
+	echo "At least one ETag does not match their url."
+	exit 1
 fi
 
 if [ -z "${ZENODO_TOKEN}" ]; then # Check Zenodo Token
-    echo "Access token not available"
-    exit 1
+	echo "Access token not available"
+	exit 1
 else
-    echo "Access token found."
+	echo "Access token found."
 fi
 
 if [ -z "${ORIGINAL_ID}" ]; then # Only get latest id when provided an original one
-    echo "Creating new deposition"
-    DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}"
+	echo "Creating new deposition"
+	DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}"
 else # Update existing dataset
-    echo "Previous ID Exists"
-    LATEST_ID=$(curl "${ZENODO_ENDPOINT}/records/${ORIGINAL_ID}/latest" |
-		    grep records | sed 's/.*href=".*\.org\/records\/\(.*\)".*/\1/')
-    REMOTE_HASH=$(curl -H "Content-Type: application/json" -X GET  --data "{}" \
-        "${DEPOSITION_PREFIX}/${LATEST_ID}/files?access_token=${ZENODO_TOKEN}" |
-		    jq ".[] .links .download" | xargs curl | md5sum | cut -f1 -d" ")
-    LOCAL_HASH=$(md5sum ${FILE_TO_VERSION} | cut -f1 -d" ")
+	echo "Previous ID Exists"
+	LATEST_ID=$(curl "${ZENODO_ENDPOINT}/records/${ORIGINAL_ID}/latest" |
+		grep records | sed 's/.*href=".*\.org\/records\/\(.*\)".*/\1/')
+	REMOTE_HASH=$(curl -H "Content-Type: application/json" -X GET --data "{}" \
+		"${DEPOSITION_PREFIX}/${LATEST_ID}/files?access_token=${ZENODO_TOKEN}" |
+		jq ".[] .links .download" | xargs curl | md5sum | cut -f1 -d" ")
+	LOCAL_HASH=$(md5sum ${FILE_TO_VERSION} | cut -f1 -d" ")
 
-    echo "Checking for changes in file contents: Remote ${REMOTE_HASH} vs Local ${LOCAL_HASH}"
-    if [ "${REMOTE_HASH}" == "${LOCAL_HASH}" ]; then
-    	  echo "The urls and md5sums have not changed"
-      	exit 0
-    fi
+	echo "Checking for changes in file contents: Remote ${REMOTE_HASH} vs Local ${LOCAL_HASH}"
+	if [ "${REMOTE_HASH}" == "${LOCAL_HASH}" ]; then
+		echo "The urls and md5sums have not changed"
+		exit 0
+	fi
 
-    echo "Creating new version"
-    DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}/${LATEST_ID}/actions/newversion"
+	echo "Creating new version"
+	DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}/${LATEST_ID}/actions/newversion"
 fi
 
 # Create new deposition
 DEPOSITION=$(curl -H "Content-Type: application/json" \
-		-X POST\
-		--data "{}" \
-		"${DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}" | \
-		jq .id)
+	-X POST --data "{}" \
+	"${DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}" |
+	jq .id)
 echo "New deposition ID is ${DEPOSITION}"
 
 # Variables
@@ -90,33 +89,32 @@ BUCKET_DATA=$(curl "${DEPOSITION_PREFIX}/${DEPOSITION}?access_token=${ZENODO_TOK
 BUCKET=$(echo "${BUCKET_DATA}" | jq --raw-output .links.bucket)
 
 if [ "${BUCKET}" = "null" ]; then
-    echo "Could not find URL for upload. Response from server:"
-    echo "${BUCKET_DATA}"
-    exit 1
+	echo "Could not find URL for upload. Response from server:"
+	echo "${BUCKET_DATA}"
+	exit 1
 fi
 
 # Upload file
 echo "Uploading file ${FILE_TO_VERSION} to bucket ${BUCKET}"
 cat ${FILE_TO_VERSION}
 curl -o /dev/null \
-    --upload-file ${FILE_TO_VERSION} \
-    "${BUCKET}"/"${FILENAME}"?access_token="${ZENODO_TOKEN}"
-
+	--upload-file ${FILE_TO_VERSION} \
+	"${BUCKET}"/"${FILENAME}"?access_token="${ZENODO_TOKEN}"
 
 # Upload Metadata
-echo -e "${METADATA_JSON}" > metadata.json
+echo -e "${METADATA_JSON}" >metadata.json
 
 NEW_DEPOSITION_ENDPOINT="${DEPOSITION_PREFIX}/${DEPOSITION}"
 echo "Uploading file to ${NEW_DEPOSITION_ENDPOINT}"
 curl -H "Content-Type: application/json" \
-    -X PUT \
-    --data @metadata.json \
-    "${NEW_DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}"
+	-X PUT \
+	--data @metadata.json \
+	"${NEW_DEPOSITION_ENDPOINT}?access_token=${ZENODO_TOKEN}"
 
 # Publish
 echo "Publishing to ${NEW_DEPOSITION_ENDPOINT}"
 curl -H "Content-Type: application/json" \
-    -X POST \
-    --data "{}" \
-    "${NEW_DEPOSITION_ENDPOINT}/actions/publish?access_token=${ZENODO_TOKEN}" | \
-    jq .id
+	-X POST \
+	--data "{}" \
+	"${NEW_DEPOSITION_ENDPOINT}/actions/publish?access_token=${ZENODO_TOKEN}" |
+	jq .id
