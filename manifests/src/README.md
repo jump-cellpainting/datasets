ec0419abfe5f6cc8f205d95a5331670d1f3db1c8
Alán F. Muñoz <amunozgo@broadinstitute.org>
2025-06-19 11:13:51 -0400

    chore: Replace profile index file format (csv->json) (#152)
    * chore: Replace profile index file format (csv->json)
    
    * change: adjust update_etag.sh to match new json structure
    
    * docs: update readme for manifest update
    
    * Update profile_index.json: specify recipe and config permalinks
    
    * docs: add permalink information
    
    ---------
    
    Co-authored-by: Shantanu Singh <shsingh@broadinstitute.org>

--- manifests/src/README.md
+++ manifests/src/README.md
@@ -1,23 +1,23 @@
 # Automated versioning with Zenodo
 
-The scripts in this folder are used for automated versioning by uploading the manifest file (`profile_index.csv`, currently the only one in the root folder) to Zenodo.
+The scripts in this folder are used for automated versioning by uploading the manifest file (`profile_index.json`, currently the only one in the root folder) to Zenodo.
 In the future, additional manifest files will be added and updated in this repository, triggering the same automated versioning process.
 
 ## Updating new versions
 
-To release a new set of assembled JUMP profiles, manually update the URLs in `profile_index.csv` to point to the new location.
-If necessary, update the associated names for new dataset types.
+To release a new set of assembled JUMP profiles, manually update the URLs in `profile_index.json` to point to the new location.
+If necessary, update the associated names for new dataset types and (optionally) the permanent link to the version of the recipe that produced them.
 
 ## Update ETags to reflect new URLs
 
 After updating a URL, the ETag (provided by S3) will no longer match. To update the ETags, run the following command from the home folder:
 
 ```bash
-bash manifests/src/update_etags.sh manifests/profile_index.csv| sponge manifests/profile_index.csv
+bash manifests/src/update_etags.sh manifests/profile_index.json | sponge manifests/profile_index.json
 ```
 
 Note: If using Nix, all dependencies are already included in the flake at the root folder. Simply run `nix develop --extra-experimental-features nix-command --extra-experimental-features flakes` before the above command.
 
 ## Commit changes
 
-Add and commit the updated `profile_index.csv`. This  should trigger an update on Zenodo. Once the update is complete, the csv files in the repository and on Zenodo should match.
+Add and commit the updated `profile_index.json`. This should trigger an update on Zenodo. Once the update is complete, the json files in the repository and on Zenodo should match.
--- manifests/src/update_etags.sh
+++ manifests/src/update_etags.sh
@@ -1,38 +1,20 @@
 #!/usr/bin/env bash
-# Fetch updated ETag values for URLs in a CSV file.
+# Fetch updated ETag values for URLs in a json file
+# Usage: `bash update_etags.sh profile_index.json`
 
-# Note that quotes are expected in the csv but ommited when
 input_file="$1"
-url_header="url"
-etag_header="etag"
 
-get_column() {
-	# gets id of column $1 in ${input_file}.
-	awk -F',' -v col="\"$1\"" 'NR==1 { for (i=1; i<=NF; ++i) { if ($i==col) print i } }' "${input_file}"
-}
+urls=$(jq .[].url "${input_file}" | tr -d '"')
 
-# Check if input file is provided
-if [ -z "${input_file}" ]; then
-	echo "Usage: $0 <input_file>"
-	exit 1
-fi
+# Pull and clean the etag from the AWS url
+NEW_ETAGS=$(printf "${urls}" | xargs -I {} sh -c "curl -I --silent {} | awk '/ETag:/ {print $2}' | cut -f2 -d' ' | tr -d '\"' | tr -d '\r'")
 
-url_column=$(get_column "${url_header}")
-urls=$(awk -F',' -v col="${url_column}" 'NR>1 {gsub(/^"|"$/, "", $col); print $col}' "${input_file}")
+# Format the list into json
+JSON_LIST=$(printf '%s\n' "${NEW_ETAGS}" | jq -R . | jq -s .)
 
-# Fetch ETags for each URL in a loop
-etag_values='"etag"'
-while IFS= read -r url; do
-	etag=$(curl -I --silent "${url}" | awk '/ETag:/ {print $2}')
-	etag_values+="\n${etag}"
-done <<<"$urls"
-
-# Remove existing ETag column if present
-etag_column=$(get_column "${etag_header}")
-
-# Combine original data (without ETag) with new ETag values
-if [[ -n "${etag_column}" ]]; then # Replace $etag_column in $input_file with $etag_values
-	awk -F',' -v OFS=',' -v col="${etag_column}" 'NR==FNR{a[NR]=$1;next}{$col=a[FNR]}1' <(echo -e "${etag_values}") "${input_file}"
-else # Append $etag_values as a new column on the right
-	paste -d',' "${input_file}" <(echo -e "${etag_values}")
-fi
+# Print again the list with the updated etag
+# Note that this assumes that every entry provides an etag
+jq --argjson etags "${JSON_LIST}" '[
+  range(0; length) as $i
+  | {subset: .[$i].subset, url: .[$i].url, recipe_permalink: .[$i].recipe_permalink, config_permalink: .[$i].config_permalink, etag: $etags[$i]}
+]' profile_index.json
